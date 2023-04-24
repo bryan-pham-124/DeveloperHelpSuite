8634948ba997853a643c4fd9a51c4e48b5eec130
bryan-pham-124 <painfuljet8@gmail.com>
2023-04-23 22:24:01 -0400

    Cleaned up code

--- app/components/DropDown.tsx
+++ app/components/DropDown.tsx
@@ -20,37 +20,31 @@ const DropDown = ({options, label, updateSort, defaultValue =''}: DropDownProps)
        <label className='text-white font-light my-2 block' htmlFor="Filter">{label}</label>
 
         {
-          updateSort &&
+          updateSort
 
-          <select  defaultValue={defaultValue} onChange = {e => updateSort(e.target.value)} className={`w-full  text-xs lg:w-[160px] bg-[url('../images/sort-down-solid.svg')] bg-no-repeat bg-[right_10%_bottom_95%] bg-[length:10px_30px] appearance-none  pl-[20px]   pb-2 pt-1  px-4  rounded-xl`}>
-          {
-              options.map((option, i) =>  (
-                <option className='text-sm rounded-xl' value={option}> {option}</option>
+          ?
 
-              ))
-          }
-        </select>
+            <select  defaultValue={defaultValue} onChange = {e => updateSort(e.target.value)} className={`w-full  text-xs lg:w-[160px] bg-[url('../images/sort-down-solid.svg')] bg-no-repeat bg-[right_10%_bottom_95%] bg-[length:10px_30px] appearance-none  pl-[20px]   pb-2 pt-1  px-4  rounded-xl`}>
+            {
+                options.map((option) =>  (
+                  <option className='text-sm rounded-xl' value={option}> {option}</option>
 
-        }
+                ))
+            }
+            </select>
 
+          :
 
-        {
-          !updateSort &&
-
-          <select  defaultValue={defaultValue}  className={`w-full  text-xs lg:w-[160px] bg-[url('../images/sort-down-solid.svg')] bg-no-repeat bg-[right_10%_bottom_95%] bg-[length:10px_30px] appearance-none  pl-[20px]   pb-2 pt-1  px-4  rounded-xl`}>
-          {
-              options.map((option, i) =>  (
-                <option className='text-sm rounded-xl' value={option}> {option}</option>
-
-              ))
-          }
-        </select>
+            <select  defaultValue={defaultValue}  className={`w-full  text-xs lg:w-[160px] bg-[url('../images/sort-down-solid.svg')] bg-no-repeat bg-[right_10%_bottom_95%] bg-[length:10px_30px] appearance-none  pl-[20px]   pb-2 pt-1  px-4  rounded-xl`}>
+            {
+                options.map((option) =>  (
+                  <option className='text-sm rounded-xl' value={option}> {option}</option>
+                ))
+            }
+           </select>
 
         }
-
-
-       
-
+        
     </>
   )
 }
