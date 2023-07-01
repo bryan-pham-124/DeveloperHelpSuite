967aefc743a8a3e563039fbe790d647ad3d4c5dd
bryan-pham-124 <painfuljet8@gmail.com>
2023-07-01 16:46:57 -0400

    Fixing W3c Id Validator Error

--- app/components/DropDown.tsx
+++ app/components/DropDown.tsx
@@ -12,6 +12,10 @@ interface DropDownProps {
 }
 
 
+const generateRandomId = () =>  Math.floor(Math.random() * (Math.floor(10000) - Math.ceil(0) + 1) + Math.ceil(1000));  
+  
+const randId = generateRandomId();
+ 
 const baseStyles = `w-full text-md lg:w-[160px] bg-[url('../images/sort-down-solid.svg')] bg-no-repeat  bg-[right_10%_bottom_95%] bg-[length:10px_30px] appearance-none  pl-[20px]   pb-2 pt-1  px-4  rounded-xl`;
 const optionStyles = 'w-full text-sm rounded-xl text-center';
 
@@ -21,7 +25,7 @@ const DropDown = ({options, label, updateSort, updateFilters, width, name = '', 
   return (
     <>
        
-       <label className='text-white font-light my-2 block' htmlFor={updateFilters ? 'Filter': updateSort ? 'Sort': 'Default'}>{label}</label>
+       <label className='text-white font-light my-2 block' htmlFor={updateFilters ? 'Filter-' + randId: updateSort ? 'Sort-'+ randId: 'Default-'+ randId}>{label}</label>
 
         {
         
@@ -30,7 +34,7 @@ const DropDown = ({options, label, updateSort, updateFilters, width, name = '', 
 
           ?
 
-            <select id= "Filter" defaultValue={defaultValue} onChange = {e => updateSort(e.target.value)} className={`w-full ${baseStyles}`}>
+            <select id= {"Filter-" + randId} defaultValue={defaultValue} onChange = {e => updateSort(e.target.value)} className={`w-full ${baseStyles}`}>
             {
                 options.map((option) =>  (
                   <option className={optionStyles} value={option}> {option}</option>
@@ -47,7 +51,7 @@ const DropDown = ({options, label, updateSort, updateFilters, width, name = '', 
 
           ?
 
-            <select id= "Sort"  defaultValue={defaultValue}  onChange = {e => updateFilters(e.target.value, label)}  className={`w-full ${baseStyles}`}>
+            <select id= {"Sort-" + randId} defaultValue={defaultValue}  onChange = {e => updateFilters(e.target.value, label)}  className={`w-full ${baseStyles}`}>
             {
                 options.map((option) =>  (
                   <option className={optionStyles} value={option}> {option !== '' ? option: '-'}</option>
@@ -59,7 +63,7 @@ const DropDown = ({options, label, updateSort, updateFilters, width, name = '', 
 
            // default dropdown
 
-           <select id="Default" name={name}  defaultValue={defaultValue}   className={`${baseStyles} w-[${width}]`}>
+           <select id={"Default-" + randId} name={name}  defaultValue={defaultValue}   className={`${baseStyles} w-[${width}]`}>
             {
                 options.map((option) =>  (
                   <option className={optionStyles} value={option}> {option !== '' ? option: '-'}</option>
